bb2e22d84ee388ac814fabb395b171c8d5ed85e1
Carlye <huangcy01@zendaimoney.com>
2018-03-07 15:36:21 +0800

    111

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -12,6 +12,7 @@ import com.example.dto.CorporateNames;
 import com.example.dto.Phones;
 import com.example.repository.CorporateRepository;
 import com.example.repository.PhonesRepository;
+import com.example.service.AsyncService;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,7 +21,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.*;
@@ -34,6 +37,9 @@ public class DemoApplicationTests {
 
 	@Autowired
     private PhonesRepository phonesRepository;
+
+	@Autowired
+	private AsyncService asyncService;
 
 	@Test
 	public void contextLoads() {
@@ -85,114 +91,46 @@ public class DemoApplicationTests {
 
 	@Test
 	public void getQxbPhone(){
-		//ExampleMatcher.matching().
-		//Page<Phones> all1 = phonesRepository.findAll(pageable);
-		int usedId = phonesRepository.findUsedId(PageRequest.of(0, 1,Sort.Direction.ASC, "corporateId")).get(0);
-		String url = "https://www.tianyancha.com/search/suggest.json?key=";
-        List<CorporateNames> all = corporateRepository.selectCorproate(usedId);
-        //all = corporateRepository.findAll();
-        int i = 0;
-        for (CorporateNames corporateNames : all) {
+		while (true){
 			try {
-				//Thread.sleep(1000L);
-				//System.out.println(url + URLUtil.encode(corporateNames.getCorporateName()));
-				//String content = HttpUtil.get(url + URLUtil.encode(corporateNames.getCorporateName()));
-				Map<String, String> params = new HashMap<>();
-				params.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-				params.put("Accept-Encoding","gzip, deflate, br");
-				params.put("Accept-Language","zh-CN,zh;q=0.8");
-				params.put("Cache-Control","no-cache");
-				params.put("Connection","keep-alive");
-				params.put("Cookie","TYCID=d3b43b90201011e8ad9175ad815931fe; undefined=d3b43b90201011e8ad9175ad815931fe; ssuid=7286007008; RTYCID=e707ee9a8d3e440997ba972d3ec2ac52; aliyungf_tc=AQAAAFAtmAk5rQMAJj7kdKH1ZS/n4XHX; csrfToken=9CkZaoOEYolyCR1PWy8G6MVl; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252213127712613%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1520229210,1520232438,1520301239,1520307238; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1520309929");
-				params.put("Host","www.tianyancha.com");
-				params.put("Pragma","no-cache");
-				params.put("Upgrade-Insecure-Requests","1");
-				params.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3964.2 Safari/537.36");
-				String content = HttpUtil.createGet(url + URLUtil.encode(corporateNames.getCorporateName())).addHeaders(params).execute().body();
-				JSONObject jsonObject = null;
+				Thread.sleep(1000L);
+			}catch (Exception e){
+
+			}
+			//ExampleMatcher.matching().
+			//Page<Phones> all1 = phonesRepository.findAll(pageable);
+			int usedId = phonesRepository.findUsedId(PageRequest.of(0, 1, Sort.Direction.ASC, "corporateId")).get(0);
+			String url = "https://www.tianyancha.com/search/suggest.json?key=";
+			List<CorporateNames> all = corporateRepository.selectCorproate(usedId);
+			//all = corporateRepository.findAll();
+			int i = 0;
+			for (CorporateNames corporateNames : all) {
 				try {
-					jsonObject = JSONUtil.parseObj(content);
-				} catch (Exception e) {
-					System.out.println(">>>>>>>>>>>>>>>>>更新了"+i+"条数据");
-					this.getQxbPhone();
-					break;
-				}
-				Object data = jsonObject.get("data");
-				JSONArray objects = JSONUtil.parseArray(data);
-				if(objects.size() == 0){
-					Phones phones = new Phones();
-					phones.setCorporateId(corporateNames.getId());
+					//Thread.sleep(1000L);
+					//System.out.println(url + URLUtil.encode(corporateNames.getCorporateName()));
+					//String content = HttpUtil.get(url + URLUtil.encode(corporateNames.getCorporateName()));
+					Map<String, String> params = new HashMap<>();
+					params.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+					params.put("Accept-Encoding","gzip, deflate, br");
+					params.put("Accept-Language","zh-CN,zh;q=0.8");
+					params.put("Cache-Control","no-cache");
+					params.put("Connection","keep-alive");
+					params.put("Cookie","TYCID=d3b43b90201011e8ad9175ad815931fe; undefined=d3b43b90201011e8ad9175ad815931fe; ssuid=7286007008; RTYCID=e707ee9a8d3e440997ba972d3ec2ac52; aliyungf_tc=AQAAAFAtmAk5rQMAJj7kdKH1ZS/n4XHX; csrfToken=9CkZaoOEYolyCR1PWy8G6MVl; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252213127712613%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1520229210,1520232438,1520301239,1520307238; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1520309929");
+					params.put("Host","www.tianyancha.com");
+					params.put("Pragma","no-cache");
+					params.put("Upgrade-Insecure-Requests","1");
+					params.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3964.2 Safari/537.36");
+					String content = HttpUtil.createGet(url + URLUtil.encode(corporateNames.getCorporateName())).addHeaders(params).execute().body();
+					JSONObject jsonObject = null;
 					try {
-						//ExampleMatcher matcher = ExampleMatcher.matching();
-						Optional<Phones> one = phonesRepository.findOne(Example.of(phones));
-						Phones phones1 = one.get();
-						if(phones1!=null){
-							phones1.setMail("未知!");
-							phonesRepository.saveAndFlush(phones1);
-						}else{
-							phones.setMail("未知!");
-							phonesRepository.saveAndFlush(phones);
-						}
+						jsonObject = JSONUtil.parseObj(content);
 					} catch (Exception e) {
+						System.out.println(">>>>>>>>>>>>>>>>>更新了"+i+"条数据");
+						break;
 					}
-                    continue;
-                }
-				Object id = JSONUtil.parseObj(objects.get(0)).get("id");
-				if(StrUtil.isNotBlank(id.toString())){
-                    String urlC = "https://www.tianyancha.com/company/";
-					Map<String, String> params1 = new HashMap<>();
-					params1.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-					params1.put("Accept-Encoding","gzip, deflate, br");
-					params1.put("Accept-Language","zh-CN,zh;q=0.8");
-					params1.put("Cache-Control","no-cache");
-					params1.put("Connection","keep-alive");
-					params1.put("Cookie","TYCID=d3b43b90201011e8ad9175ad815931fe; undefined=d3b43b90201011e8ad9175ad815931fe; ssuid=7286007008; RTYCID=e707ee9a8d3e440997ba972d3ec2ac52; aliyungf_tc=AQAAAFAtmAk5rQMAJj7kdKH1ZS/n4XHX; csrfToken=9CkZaoOEYolyCR1PWy8G6MVl; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252213127712613%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1520229210,1520232438,1520301239,1520307238; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1520309929");
-					params1.put("Host","www.tianyancha.com");
-					params1.put("Pragma","no-cache");
-					params1.put("Upgrade-Insecure-Requests","1");
-					params1.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3964.2 Safari/537.36");
-					String content1 = HttpUtil.createGet(urlC + id.toString()).addHeaders(params1).execute().body();
-                    Document parse = Jsoup.parse(content1, "utf-8");
-                    Elements select = parse.select("div.f14.sec-c2>div>span");
-                    //Elements select = select1.select("span");
-                    String phone = "";
-                    String mail = "";
-                    String regex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$";
-                    String parent = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
-                    for (Element element : select) {
-						if (StrUtil.equalsIgnoreCase("查看更多",element.text())){
-                            Elements script = select.select("script");
-                            phone = script.html();
-                        }
-                        if(element.text().contains("021") || element.text().matches(regex)){
-                                phone = element.text();
-                        }
-						if(element.html().matches(parent)){
-							mail = element.html();
-						}
-                    }
-                    if (StrUtil.isBlank(mail)){
-                    	mail="未知!";
-					}
-                    if (StrUtil.isNotBlank(phone)){
-                        Phones phones = new Phones();
-                        phones.setCorporateId(corporateNames.getId());
-						try {
-							//ExampleMatcher matcher = ExampleMatcher.matching();
-							Optional<Phones> one = phonesRepository.findOne(Example.of(phones));
-							Phones phones1 = one.get();
-							if(phones1!=null){
-								phones1.setPhone(phone);
-								phones1.setMail(mail);
-								phonesRepository.saveAndFlush(phones1);
-							}else{
-								phones.setPhone(phone);
-								phones.setMail(mail);
-								phonesRepository.saveAndFlush(phones);
-							}
-						} catch (Exception e) {
-						}
-					}else {
+					Object data = jsonObject.get("data");
+					JSONArray objects = JSONUtil.parseArray(data);
+					if(objects.size() == 0){
 						Phones phones = new Phones();
 						phones.setCorporateId(corporateNames.getId());
 						try {
@@ -208,14 +146,88 @@ public class DemoApplicationTests {
 							}
 						} catch (Exception e) {
 						}
+						continue;
 					}
-                }
-			} catch (UtilException e) {
+					Object id = JSONUtil.parseObj(objects.get(0)).get("id");
+					if(StrUtil.isNotBlank(id.toString())){
+						String urlC = "https://www.tianyancha.com/company/";
+						Map<String, String> params1 = new HashMap<>();
+						params1.put("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+						params1.put("Accept-Encoding","gzip, deflate, br");
+						params1.put("Accept-Language","zh-CN,zh;q=0.8");
+						params1.put("Cache-Control","no-cache");
+						params1.put("Connection","keep-alive");
+						params1.put("Cookie","TYCID=d3b43b90201011e8ad9175ad815931fe; undefined=d3b43b90201011e8ad9175ad815931fe; ssuid=7286007008; RTYCID=e707ee9a8d3e440997ba972d3ec2ac52; aliyungf_tc=AQAAAFAtmAk5rQMAJj7kdKH1ZS/n4XHX; csrfToken=9CkZaoOEYolyCR1PWy8G6MVl; tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252213127712613%2522%257D; auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzEyNzcxMjYxMyIsImlhdCI6MTUyMDMwNzIzNSwiZXhwIjoxNTM1ODU5MjM1fQ.1KRN5N0LA4Io-D25kbYAVZsDPdQZrTGHl1cSJ-5YyUjzNGaUYgYBvXC4eCfCrdwUdzVw3RdfSb20SbdJiGNe2A; Hm_lvt_e92c8d65d92d534b0fc290df538b4758=1520229210,1520232438,1520301239,1520307238; Hm_lpvt_e92c8d65d92d534b0fc290df538b4758=1520309929");
+						params1.put("Host","www.tianyancha.com");
+						params1.put("Pragma","no-cache");
+						params1.put("Upgrade-Insecure-Requests","1");
+						params1.put("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 UBrowser/6.2.3964.2 Safari/537.36");
+						String content1 = HttpUtil.createGet(urlC + id.toString()).addHeaders(params1).execute().body();
+						Document parse = Jsoup.parse(content1, "utf-8");
+						Elements select = parse.select("div.f14.sec-c2>div>span");
+						//Elements select = select1.select("span");
+						String phone = "";
+						String mail = "";
+						String regex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$";
+						String parent = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+						for (Element element : select) {
+							if (StrUtil.equalsIgnoreCase("查看更多",element.text())){
+								Elements script = select.select("script");
+								phone = script.html();
+							}
+							if(element.text().contains("021") || element.text().matches(regex)){
+								phone = element.text();
+							}
+							if(element.html().matches(parent)){
+								mail = element.html();
+							}
+						}
+						if (StrUtil.isBlank(mail)){
+							mail="未知!";
+						}
+						if (StrUtil.isNotBlank(phone)){
+							Phones phones = new Phones();
+							phones.setCorporateId(corporateNames.getId());
+							try {
+								//ExampleMatcher matcher = ExampleMatcher.matching();
+								Optional<Phones> one = phonesRepository.findOne(Example.of(phones));
+								Phones phones1 = one.get();
+								if(phones1!=null){
+									phones1.setPhone(phone);
+									phones1.setMail(mail);
+									phonesRepository.saveAndFlush(phones1);
+								}else{
+									phones.setPhone(phone);
+									phones.setMail(mail);
+									phonesRepository.saveAndFlush(phones);
+								}
+							} catch (Exception e) {
+							}
+						}else {
+							Phones phones = new Phones();
+							phones.setCorporateId(corporateNames.getId());
+							try {
+								//ExampleMatcher matcher = ExampleMatcher.matching();
+								Optional<Phones> one = phonesRepository.findOne(Example.of(phones));
+								Phones phones1 = one.get();
+								if(phones1!=null){
+									phones1.setMail("未知!");
+									phonesRepository.saveAndFlush(phones1);
+								}else{
+									phones.setMail("未知!");
+									phonesRepository.saveAndFlush(phones);
+								}
+							} catch (Exception e) {
+							}
+						}
+					}
+				} catch (UtilException e) {
 
-			} catch (Exception e) {
-				e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				i++;
 			}
-			i++;
 		}
     }
 
